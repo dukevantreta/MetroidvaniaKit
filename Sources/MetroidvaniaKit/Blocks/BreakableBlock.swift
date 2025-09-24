78ddbae6284fe62175e8a73f16c75491a13ec132
Duke Van Treta <dukevantreta@icloud.com>
2025-09-24 20:57:37 -0300

    Removed breakable block deinit message

--- Sources/MetroidvaniaKit/Blocks/BreakableBlock.swift
+++ Sources/MetroidvaniaKit/Blocks/BreakableBlock.swift
@@ -40,8 +40,4 @@ class BreakableBlock: RigidBody2D {
     func reveal() {
         coverSprite?.visible = false
     }
-    
-    deinit {
-        GD.print("BreakableBlock deinitialized")
-    }
 }
